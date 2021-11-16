fe58569395fb16be72c137ecbbcb744a6182da04
Maksym Zuliak <agrh.ndcity@gmail.com>
2021-11-16 12:14:32 +0200

    changed def parse_parameters and add def parse_cookies

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 def parse_parameters(query: str) -> dict:
     result = {}
-    temp_str = query[query.find('?'):] if query.find('?') != -1 else ''
+    count = query.count('=')
+    query = query[query.find('?') + 1:] if query.find('?') != -1 else ''
 
-    for number in range(temp_str.count('=')):
-        key = temp_str[temp_str.find('?') + 1:temp_str.find('=')]
-        item = temp_str[temp_str.find('=') + 1:temp_str.find('&')] if number + 1 != query.count('=') \
+    for number in range(count):
+        key = query[:query.find('=')]
+        item = query[query.find('=') + 1:query.find('&')] if number + 1 != count \
             else query[query.rfind('=') + 1:]
         result[key] = item
-        temp_str = '?' + temp_str[temp_str.find('&') + 1:] if number + 1 != query.count('=') else ''
+        query = query[query.find('&') + 1:] if number + 1 != count else ''
 
     return result
 
 
 def parse_cookies(query: str) -> dict:
     result = {}
+    count = query.count('=')
+
+    for number in range(count):
+        key = query[:query.find('=')]
+        item = query[query.find('=') + 1:query.find(';')] if number + 1 != count \
+            else query[query.find('=') + 1:] if query[-1] != ';' else query[query.find('=') + 1: -1]
+        result[key] = item
+        query = query[query.find(';') + 1:] if number + 1 != count else ''
+
     return result
 
 
@@ -28,3 +38,6 @@ if __name__ == '__main__':
     # Tests for function "parse_cookies"
     assert parse_cookies('') == {}
     assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookies('ipaddress=179.12.011.19;browser=GoogleChrome') == {'ipaddress': '179.12.011.19', 'browser': 'GoogleChrome'}
+    assert parse_cookies('date=12/12/2020;time=18:33:20;country=Ukraine') == {'date': '12/12/2020', 'time': '18:33:20', 'country': 'Ukraine'}
+    assert parse_cookies('first=0000001;second=0000002;') == {'first': '0000001', 'second': '0000002'}
